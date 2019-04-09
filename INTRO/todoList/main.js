c4a93ab85d07015e015dccd8da39fb4655151657
kaede0902 <kaede0902js@gmail.com>
2019-04-09 20:47:58 +0900

    update todoList

---
+++ INTRO/todoList/main.js
@@ -0,0 +1,13 @@
+const app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { task: 'Learn JS' },
+        { task: 'Learn Vue' },
+        { task: 'Learn obj' },
+        { task: 'Make portfolio' },
+        { task: 'Make Recive sys' }
+      ]
+    },
+
+})
